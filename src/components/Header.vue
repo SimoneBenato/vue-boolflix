b435f2d79bb76369142103dc6b620a6822e6946b
SimoneBenato <sb270298@gmail.com>
2021-09-24 15:38:37 +0200

    Creazione scaffholding

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container mb-5">
+    <div class="row d-flex justify-content-between align-items-center p-1">
+      <div class="col-3">
+        <h1 class="fw-bold fs-3">BOOLFLIX</h1>
+      </div>
+      <div class="col-3">
+        <input type="text" placeholder="Serch a movie">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-3"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+}
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
